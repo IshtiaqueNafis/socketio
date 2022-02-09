6ad171492a515964695e6e8236a01fe38e9fa40c
IshtiaqueNafis <nafu22@gmail.com>
2022-02-08 19:29:43 -0500

    set up socket.io

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const path = require('path');
-
+const http = require('http'); // http
 const express = require('express');
+const socketIo = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server); // server is passed on here.
+
+
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, './public');
 
 app.use(express.static(publicDirectoryPath)) // will let use public stuff.
 
-app.listen(port, ()=>{
+
+let count = 0;
+io.on('connection',()=>{
+    console.log('new websocket connection')
+})
+
+server.listen(port, () => {
     console.log(`Server is running on ${port}`);
 })
